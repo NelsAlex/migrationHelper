4bcca0c1431ca316348d0230d6669e661157a533
Feyza-S <f.rosesanal@gmail.com>
2025-07-29 09:05:28 -0700

    Create Disable-COTWiFi.ps1

---
+++ Disable-COTWiFi.ps1
@@ -0,0 +1,29 @@
+# Script: Disable-COTWiFi.ps1
+
+# Set adapter name (exact or partial match)
+$targetAdapterName = "cotwifi"
+
+# Get the adapter with that name
+$adapter = Get-NetAdapter | Where-Object { $_.Name -like "*$targetAdapterName*" }
+
+# Check if adapter exists
+if ($null -eq $adapter) {
+    Write-Output "No network adapter found matching '*$targetAdapterName*'."
+    return
+}
+
+# Disable if it's up
+if ($adapter.Status -eq "Up") {
+    Disable-NetAdapter -Name $adapter.Name -Confirm:$false
+    Write-Output "Disabled adapter: $($adapter.Name)"
+} else {
+    Write-Output "Adapter is already disabled or not active: $($adapter.Name)"
+}
+
+
+How to Test:
+Save as Disable-COTWiFi.ps1
+
+Run in PowerShell as Administrator
+
+Output will tell you whether it found and disabled the adapter!
