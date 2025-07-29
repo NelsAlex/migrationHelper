10af975db0d9a25fd5a8e44ca6e08c3e43c0a3e6
Feyza-S <f.rosesanal@gmail.com>
2025-07-29 09:08:29 -0700

    Update and rename Enable-COTWIFI to Enable-COTWIFI.ps1

--- Enable-COTWIFI.ps1
+++ Enable-COTWIFI.ps1
@@ -3,6 +3,11 @@
 $targetAdapterName = "COTWiFi"
 $adapter = Get-NetAdapter | Where-Object { $_.Name -like "*$targetAdapterName*" }
 
+if ($null -eq $adapter) {
+    Write-Output "No network adapter found matching '*$targetAdapterName*'."
+    return
+}
+
 if ($adapter.Status -eq "Disabled") {
     Enable-NetAdapter -Name $adapter.Name -Confirm:$false
     Write-Output "Enabled adapter: $($adapter.Name)"
